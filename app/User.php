f800ff60e35c6afe86ead07fdfa3bbc5b87d196b
NashPL <nash25pl@gmail.com>
2019-07-26 22:42:49 +0100

    Added table name for the class.

--- app/User.php
+++ app/User.php
@@ -16,6 +16,8 @@ class User
     private $country;
     private $validated;
 
+    private $tableName = "users";
+
     /**
      * A class constructor desinged to build a user object.
      *
@@ -25,13 +27,13 @@ class User
      */
     public function __construct(string $email, array $userDetails)
     {
-        $user = DB::table('users')->where('_email', $email)->first();
+        $user = DB::table($this->tableName)->where('_email', $email)->first();
         if (is_null($user) || empty($user)) {
             $this->email = $email;
             $this->password = password_hash($userDetails['password'], PASSWORD_BCRYPT);
             $this->country = $userDetails['country'];
             $this->validated = false;
-            $this->id = DB::table('users')->insert(
+            $this->id = DB::table($this->tableName)->insert(
                 ['_email' => $this->email, '_password' => $this->password,
                     '_country' => $this->country, '_validated' => $this->validated]
             );
